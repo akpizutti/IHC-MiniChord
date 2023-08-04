ec9a253290ce9eb8d7010c25c8612cb67f195b9c
akpizutti <akpizutti@gmail.com>
2023-08-04 01:33:13 -0300

    Added supersaw sound

--- app/src/main/cpp/Oscillator.cpp
+++ app/src/main/cpp/Oscillator.cpp
@@ -4,16 +4,16 @@
 
 #include "Oscillator.h"
 #include <math.h>
+#include <android/log.h>
 
-#define TWO_PI (3.14159 * 2)
-#define AMPLITUDE 0.2
-#define TUNING 440.0
-#define NOTE 3.0
-#define ORGAN_HARMONICS 4
+
 
 void Oscillator::setSampleRate(int32_t sampleRate) {
     phaseIncrement_ = (TWO_PI * TUNING) / (double) sampleRate;
     nyquist = float(sampleRate / 2.0);
+    initializePhases();
+
+
 }
 
 void Oscillator::setWaveOn(bool isWaveOn, int notes[]) {
@@ -26,8 +26,17 @@ void Oscillator::setWaveOn(bool isWaveOn, int notes[]) {
 void Oscillator::render(float *audioData, int32_t numFrames) {
 
     if (!isWaveOn_.load()){
-        for(int i = 0; i < MAX_POLYPHONY; i++)
+        for(int i = 0; i < MAX_POLYPHONY; i++) {
             phase_[i] = 0.0;
+            for(int j=0; j<7; j++){
+                //code taken from https://stackoverflow.com/questions/7560114/random-number-c-in-some-range
+                std::random_device rd; // obtain a random number from hardware
+                std::mt19937 gen(rd()); // seed the generator
+                std::uniform_int_distribution<> distr(TWO_PI*-1, TWO_PI); // define the range
+                supersaw_phase_[i][j] = distr(gen); //phase randomization so that supersaw sounds good
+            }
+        }
+
     }
 
     for (int i = 0; i < numFrames; i++) {
@@ -39,20 +48,20 @@ void Oscillator::render(float *audioData, int32_t numFrames) {
 
             audioData[i] = 0.0;
             float currentSample = 0.0;
-            // Increments the phase, handling wrap around.
-            // f = f0 * (2^(n/12))
+
 
             for(int k=0; k<MAX_POLYPHONY; k++){
                 if(notes_[k] >= NOTE_C_0){
 
                     //simple sine wave
-                    /*currentSample += (float) (sin(phase_[k] * 2)) * AMPLITUDE ;*/
+                    //currentSample += (float) (sin(phase_[k] * 16)) * AMPLITUDE ;
 
                     //tonewheel organ sound
                     for(int j=0; j < ORGAN_HARMONICS; j++) {
                         currentSample += (float) (sin((phase_[k] * pow(2, j)) ) * AMPLITUDE *
                                                   decibel2linear(j*-3.0));
                     }
+
 
                     //attempting to recreate a sound I stumbled upon in FL Studio's 3xOsc that I thought kinda resembles a Rhodes.
                     /*
@@ -63,14 +72,27 @@ void Oscillator::render(float *audioData, int32_t numFrames) {
                     currentSample = currentSample *( (float) sin((phase_[k]))  * decibel2linear(-4.0) );
                     */
 
-                    //another sound I stumbled upon while writing this code, idk how, but it sounds like a sawtooth
-                    /*currentSample += (float) sin((phase_[k]) /2 ) * AMPLITUDE;
-                    currentSample += (float) sin((phase_[k]) /4 ) * AMPLITUDE * decibel2linear(-4);*/
+                    //single sawtooth
+                    //currentSample += phase_[k] * decibel2linear(-6.0);
 
-                    audioData[i] += currentSample * decibel2linear(-3.0); //attenuate the signal so it doesn't clip
+                    //supersaw
+                    /*for(int j=0;j<UNISON_VOICES; j++){
+                        currentSample += supersaw_phase_[k][j] * decibel2linear(-18.0);
 
-                    phase_[k] += phaseIncrement_ * ((pow(double(2.0),double(double(notes_[k])/12))));
+                        supersaw_phase_[k][j] += ( 2* phaseIncrement_ + ((j-(j/2))*0.0004)) * ((pow(double(2.0),double(double(notes_[k])/12))));
+                        if (supersaw_phase_[k][j] > TWO_PI)
+                            supersaw_phase_[k][j] -= TWO_PI;
+                    }*/
+
+                    currentSample = currentSample * decibel2linear(-6.0); //attenuate the signal to avoid clipping
+                    audioData[i] += currentSample;
+
+                    // Increments the phase, handling wrap around.
+                    // f = f0 * (2^(n/12))
+                    phase_[k] += (phaseIncrement_ ) * ((pow(double(2.0),double(double(notes_[k])/12))));
+                    //phase_[k] += phase_[k] / 4;
                     if (phase_[k] > TWO_PI) phase_[k] -= TWO_PI;
+
                 }
             }
 
@@ -85,4 +107,14 @@ void Oscillator::render(float *audioData, int32_t numFrames) {
 
 float Oscillator::decibel2linear(float decibels){
     return pow(float(10.0),float(decibels/10.0));
+}
+
+void Oscillator::initializePhases() {
+    for(int i=0;i<MAX_POLYPHONY;i++){
+        phase_[i] = 0.0;
+
+        for(int j=0; j<7; j++){
+            supersaw_phase_[i][j] = 0.0;
+        }
+    }
 }
--- app/src/main/cpp/Oscillator.h
+++ app/src/main/cpp/Oscillator.h
@@ -8,9 +8,17 @@
 
 #include <atomic>
 #include <stdint.h>
+#include <android/log.h>
 #include "Notes.h"
+#include <random>
 
-
+#define TWO_PI (3.14159 * 2)
+#define AMPLITUDE 0.2
+#define TUNING 440.0
+#define NOTE 3.0
+#define ORGAN_HARMONICS 4
+#define UNISON_VOICES 7
+#define DETUNE 0.1
 
 class Oscillator {
 public:
@@ -20,11 +28,14 @@ public:
 
 private:
     std::atomic<bool> isWaveOn_{false};
-    double phase_[MAX_POLYPHONY] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    double phase_[MAX_POLYPHONY] ;
     double phaseIncrement_ = 0.0;
     int notes_[MAX_POLYPHONY] = {NOTE_NULL,NOTE_NULL,NOTE_NULL,NOTE_NULL,NOTE_NULL,NOTE_NULL};
     float nyquist;
     float decibel2linear(float decibels);
+
+    double supersaw_phase_[MAX_POLYPHONY][UNISON_VOICES];
+    void initializePhases();
 };
 
 
