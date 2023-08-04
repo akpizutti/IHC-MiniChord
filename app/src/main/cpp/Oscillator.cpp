48419d29722047cdeb0b60ed458d2850594cd908
akpizutti <akpizutti@gmail.com>
2023-08-04 00:02:59 -0300

    Changed default sound to organ sound

--- app/src/main/cpp/Oscillator.cpp
+++ app/src/main/cpp/Oscillator.cpp
@@ -9,7 +9,7 @@
 #define AMPLITUDE 0.2
 #define TUNING 440.0
 #define NOTE 3.0
-#define HARMONICS 10
+#define ORGAN_HARMONICS 4
 
 void Oscillator::setSampleRate(int32_t sampleRate) {
     phaseIncrement_ = (TWO_PI * TUNING) / (double) sampleRate;
@@ -38,31 +38,36 @@ void Oscillator::render(float *audioData, int32_t numFrames) {
             //audioData[i] = (float) (sin(phase_) * AMPLITUDE);
 
             audioData[i] = 0.0;
-            float temp = 0.0;
+            float currentSample = 0.0;
             // Increments the phase, handling wrap around.
             // f = f0 * (2^(n/12))
 
             for(int k=0; k<MAX_POLYPHONY; k++){
                 if(notes_[k] >= NOTE_C_0){
-                    /*for(int j=0; j < HARMONICS; j++) {
-                        audioData[i] += (float) (sin((phase_[k] * pow(2,j)) /2 ) * AMPLITUDE *
-                                decibel2linear(j*-3.0));
-                    }*/
+
+                    //simple sine wave
+                    /*currentSample += (float) (sin(phase_[k] * 2)) * AMPLITUDE ;*/
+
+                    //tonewheel organ sound
+                    for(int j=0; j < ORGAN_HARMONICS; j++) {
+                        currentSample += (float) (sin((phase_[k] * pow(2, j)) ) * AMPLITUDE *
+                                                  decibel2linear(j*-3.0));
+                    }
 
                     //attempting to recreate a sound I stumbled upon in FL Studio's 3xOsc that I thought kinda resembles a Rhodes.
                     /*
                     //two sine waves added together:
-                    temp += (float) (sin((phase_[k])) *2.0 ) * AMPLITUDE;
-                    temp += (float) (sin((phase_[k]))   ) * decibel2linear(-4.0) * AMPLITUDE;
+                    currentSample += (float) (sin((phase_[k])) *2.0 ) * AMPLITUDE;
+                    currentSample += (float) (sin((phase_[k]))   ) * decibel2linear(-4.0) * AMPLITUDE;
                     //then multiplied by another sine, also known as amplitude modulation (AM):
-                    temp = temp *( (float) sin((phase_[k]))  * decibel2linear(-4.0) );
+                    currentSample = currentSample *( (float) sin((phase_[k]))  * decibel2linear(-4.0) );
                     */
 
                     //another sound I stumbled upon while writing this code, idk how, but it sounds like a sawtooth
-                    audioData[i] += (float) sin((phase_[k]) /2 ) * AMPLITUDE;
-                    audioData[i] += (float) sin((phase_[k]) /4 ) * AMPLITUDE * decibel2linear(-4);
+                    /*currentSample += (float) sin((phase_[k]) /2 ) * AMPLITUDE;
+                    currentSample += (float) sin((phase_[k]) /4 ) * AMPLITUDE * decibel2linear(-4);*/
 
-                    audioData[i] += temp;
+                    audioData[i] += currentSample * decibel2linear(-3.0); //attenuate the signal so it doesn't clip
 
                     phase_[k] += phaseIncrement_ * ((pow(double(2.0),double(double(notes_[k])/12))));
                     if (phase_[k] > TWO_PI) phase_[k] -= TWO_PI;
